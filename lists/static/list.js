06e9cb8cff54e848a59d7b80616d10b2d1213281
Patrick Godschalk <patrick@kernelpanics.nl>
2023-03-30 10:35:42 +0200

    feat(chapter16): hide click listener

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,6 +1,10 @@
-// eslint-disable-next-line no-unused-vars
-const initialize = function () {
+window.Superlists = {}
+window.Superlists.initialize = function () {
   $('input[name="text"]').on("keypress", function () {
+    $(".has-error").hide()
+  })
+
+  $('input[name="text"]').on("click", function () {
     $(".has-error").hide()
   })
 }
