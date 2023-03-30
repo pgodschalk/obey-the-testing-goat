9c7985a7494d3765e74da0e4c4d521f121b21321
Patrick Godschalk <patrick@kernelpanics.nl>
2023-03-30 09:08:25 +0200

    style(chapter15): method ordering

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -31,6 +31,12 @@ class ListViewTest(TestCase):
         response = self.client.get(f"/lists/{correct_list.id}/")
         self.assertEqual(response.context["list"], correct_list)
 
+    def test_displays_item_form(self):
+        list_ = List.objects.create()
+        response = self.client.get(f"/lists/{list_.id}/")
+        self.assertIsInstance(response.context["form"], ExistingListItemForm)
+        self.assertContains(response, 'name="text"')
+
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
         Item.objects.create(text="itemey 1", list=correct_list)
@@ -69,12 +75,6 @@ class ListViewTest(TestCase):
             data={"text": "A new item for an existing list"},
         )
         self.assertRedirects(response, f"/lists/{correct_list.id}/")
-
-    def test_displays_item_form(self):
-        list_ = List.objects.create()
-        response = self.client.get(f"/lists/{list_.id}/")
-        self.assertIsInstance(response.context["form"], ExistingListItemForm)
-        self.assertContains(response, 'name="text"')
 
     def post_invalid_input(self):
         list_ = List.objects.create()
