f1e58952bcb38f73479fc8c92afddc704ee21fab
Patrick Godschalk <patrick@kernelpanics.nl>
2023-04-24 14:33:26 +0200

    test(chapter21): server-side debugging

---
+++ functional_tests/server_tools.py
@@ -0,0 +1,28 @@
+from fabric.api import run
+from fabric.context_managers import settings, shell_env
+
+
+def _get_manage_dot_py(host):
+    return (
+        f"podman exec {host}devkernelpanicslnl_obey-the-testing-goat_1 python manage.py"
+    )
+
+
+def reset_database(host):
+    manage_dot_py = _get_manage_dot_py(host)
+    with settings(host_string=f"patrick@{host}"):
+        run(f"{manage_dot_py} flush --noinput")
+
+
+def _get_server_env_vars(host):
+    env_lines = run(f"cat ~/{host}.dev.kernelpanics.nl/.env").splitlines()
+    return dict(line.split("=") for line in env_lines if line)
+
+
+def create_session_on_server(host, email):
+    manage_dot_py = _get_manage_dot_py(host)
+    with settings(host_string=f"patrick@{host}"):
+        env_vars = _get_server_env_vars(host)
+        with shell_env(**env_vars):
+            session_key = run(f"{manage_dot_py} create_session {email}")
+            return session_key.strip()
