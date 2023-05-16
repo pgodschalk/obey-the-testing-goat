28c904f26e96ed16406ef46b1aefced585201a09
Patrick Godschalk <patrick@kernelpanics.nl>
2023-05-16 18:01:43 +0200

    ci(chapter24): add screenshot on failure to FT runner

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
@@ -7,6 +8,9 @@ from selenium.webdriver.common.keys import Keys
 from .server_tools import reset_database
 
 MAX_WAIT = 10
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "screendumps"
+)
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -18,7 +22,20 @@ class FunctionalTest(StaticLiveServerTestCase):
             reset_database(self.staging_server)
 
     def tearDown(self):
+        if self._test_has_failed():
+            if not os.path.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.browser.switch_to_window(handle)
+                self.take_screenshot()
+                self.dump_html()
         self.browser.quit()
+        super().tearDown()
+
+    def _test_has_failed(self):
+        # slightly obscure but couldn't find a better way!
+        return any(error for (method, error) in self._outcome.errors)
 
     def wait(fn):
         def modified_fn(*args, **kwargs):
@@ -64,3 +81,24 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.get_item_input_box().send_keys(Keys.ENTER)
         item_number = num_rows + 1
         self.wait_for_row_in_list_table(f"{item_number}: {item_text}")
+
+    def take_screenshot(self):
+        filename = self._get_filename() + ".png"
+        print("screenshotting to", filename)
+        self.browser.get_screenshot_as_file(filename)
+
+    def dump_html(self):
+        filename = self._get_filename() + ".html"
+        print("dumping page HTML to", filename)
+        with open(filename, "w") as f:
+            f.write(self.browser.page_source)
+
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(":", ".")[:19]
+        return "{folder}/{classname}.{method}-window{windowid}-{timestamp}".format(
+            folder=SCREEN_DUMP_LOCATION,
+            classname=self.__class__.__name__,
+            method=self._testMethodName,
+            windowid=self._windowid,
+            timestamp=timestamp,
+        )
