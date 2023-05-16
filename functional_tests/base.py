45a293575296ab984c38fb92de89039c4c78658e
Patrick Godschalk <patrick@kernelpanics.nl>
2023-05-16 20:13:52 +0200

    test(appendix-e): behaviour-driven development (BDD)

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -9,6 +9,20 @@ from .server_tools import reset_database
 MAX_WAIT = 10
 
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,19 +33,6 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
-
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-
-        return modified_fn
 
     @wait
     def wait_for_row_in_list_table(self, row_text):
--- superlists/settings.py
+++ superlists/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "lists",
     "accounts",
     "functional_tests",
+    "behave_django",
 ]
 
 AUTH_USER_MODEL = "accounts.User"
